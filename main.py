b15ceef94d72ed381dfaed8f7950ef6aa1a84f6e
Arpan Ghosh <114988864+ArGhoCodes@users.noreply.github.com>
2024-02-20 11:19:59 +0000

    updated duration level for model improvement

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ while True:
 
         with sr.Microphone() as mic:
 
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            recognizer.adjust_for_ambient_noise(mic, duration=1)
             audio = recognizer.listen(mic)
 
             text = recognizer.recognize_google(audio, language='en')
