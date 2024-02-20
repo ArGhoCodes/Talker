f384dad6c361aef598caf20ba835207c9dce536b
Arpan Ghosh <114988864+ArGhoCodes@users.noreply.github.com>
2024-02-20 10:33:23 +0000

    speech to text working fine

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import speech_recognition as sr
 import pyttsx3
 
 recognizer = sr.Recognizer()
+while True:
+
+    try:
+
+        with sr.Microphone() as mic:
+
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio, language='en')
+            text = text.lower()
+
+            print(f'Recognized speech to text:  {text}')
+
+    except sr.UnknownValueError():
+        recognizer = sr.Recognizer()
+        continue
